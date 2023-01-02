711cd318f954f9bb32d9ff131c01980c08a75e5f
Ислам Таштанбеков <islamtastanbekov@MacBook-Air-Islam.local>
2023-01-02 17:22:10 +0600

    fill profile

--- src/components/Profile/MyPosts/Posts/MyPosts.jsx
+++ src/components/Profile/MyPosts/Posts/MyPosts.jsx
@@ -6,10 +6,14 @@ const MyPosts = () => {
       <div className={s.myPosts}>
           My posts
           <div>
-              <div>New post</div>
+              <div><img src="https://i.ibb.co/285sgtC/iphone-14-pro-finish-unselect-gallery-1-202209-GEO-US-removebg-1.png"/>></div>
+          </div>
+          <div className={s.item}>
+
+                  Add like
           </div>
           <div className={s.item}>Post 1</div>
-          <div className={s.item}>Post 1</div>
+          <input type='checkbox' />
       </div>
     )
 }
