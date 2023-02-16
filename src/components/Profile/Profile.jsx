2d02189438763c3c7a7b84bfa02292409633fafe
Isuu_tm <temirovislam10@gmail.com>
2023-02-16 08:42:03 +0600

    Hoc React Memo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
-const Profile = (props) => {
-    return (
-        <nav className={s.profile}>
-            <MyPosts />
-            <ProfileInfo status={props.status} updateStatus={props.updateStatus} />
-        </nav>
-    )
-}
+const Profile =  React.memo((props) => {
+        return (
+            <nav className={s.profile}>
+                <MyPosts />
+                <ProfileInfo status={props.status} updateStatus={props.updateStatus} />
+            </nav>
+        )
+})
 
 
 export default Profile;
